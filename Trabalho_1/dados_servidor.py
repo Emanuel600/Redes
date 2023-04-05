8c72699f969e1827b68a6f236f1bc454ab30846e
Emanuel <78040463+Emanuel600@users.noreply.github.com>
2023-04-05 09:48:43 -0300

    Create dados_servidor.py

---
+++ Trabalho_1/dados_servidor.py
@@ -0,0 +1,12 @@
+"""
+  Define um dicionário que parea os códigos com seus respectivos
+nomes e níveis de acesso. Desta forma a lista fica simples de acessar
+e pode ser expandida ou retraída com facilidade
+"""
+codigos = {
+    "14588"   : ["Elend Venture", '5'],
+    "4287"    : ["Edgard Ladrian", '4'],
+    "4298"    : ["Vin", '2'],
+    "6528"    : ["Lord Renoux", '3'],
+    "7458"    : ["Kelsier", '1']
+    }
